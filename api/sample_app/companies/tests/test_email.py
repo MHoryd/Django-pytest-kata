282bf9ea449724f2ebdc077c07a2a1aa7a8cdb8b
MHoryd <michal.horyd@gmail.com>
2025-07-30 20:40:15 +0200

    email unittests

---
+++ api/sample_app/companies/tests/test_email.py
@@ -0,0 +1,34 @@
+import json
+from unittest.mock import patch
+from django.test import Client
+from api.sample_app.companies.views import send_company_email
+
+client = Client()
+
+
+@patch('api.sample_app.companies.views.requests.post')
+def test_proper_response_on_success(mock_post):
+    mock_post.return_value.status_code = 200
+
+    payload = {"subject": "Test", "message": "Hello"}
+    response = client.post('/send-email', data=payload)
+
+    assert response.status_code == 200
+    assert response.json() == {"status":"success", "info":"email send successfully"}
+
+
+@patch('api.sample_app.companies.views.requests.post')
+def test_proper_response_on_failure(mock_post):
+    mock_post.return_value.status_code = 400
+    
+    payload = {"subject": "Test", "message": "Hello"}
+    response = client.post('/send-email', data=payload)
+    
+    assert response.status_code == 400
+    assert response.json() == {"status":"failure", "info":"email not send successfully"}
+
+
+def test_get_method_should_fail():
+    response = client.get('/send-email')
+    assert response.status_code == 405
+    assert json.loads(response.content) == {"detail": "Method \"GET\" not allowed."}
